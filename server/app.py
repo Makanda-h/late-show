d88263951793b6dd9b6a3dc29e5fe3d69bc6b030
Hope <hopemwaura39@gmail.com>
2024-10-16 13:08:43 +0300

    create tables for models

--- server/app.py
+++ server/app.py
@@ -1,8 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_restful import Api, Resource
+from models import db
+from flask_migrate import Migrate
 
 #create flask app
 app = Flask(__name__)
+
+#configure app
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
 #initialize
 api = Api(app)
 
